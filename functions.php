da08c4d7788ba4470355a448a1213442e27519b1
Sander Langendoen <s.langendoen@wpguitgevers.nl>
2026-02-04 13:27:27 +0100

    first commit

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+// Child theme scripts/styles laden
+function twentytwentyfour_child_enqueue_styles() {
+    // Parent theme CSS
+    wp_enqueue_style(
+        'twentytwentyfour-style',
+        get_template_directory_uri() . '/style.css'
+    );
+
+    // Child theme CSS
+    wp_enqueue_style(
+        'twentytwentyfour-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('twentytwentyfour-style'),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action('wp_enqueue_scripts', 'twentytwentyfour_child_enqueue_styles');
